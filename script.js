dd2bcf37dcbad47a5ad475bfc0e5b344ad434b0e
Shahid Ameen <43674745+iamshahidameen@users.noreply.github.com>
2022-04-04 22:42:25 +0500

    added a function to add the values in history and finds if the item is negative or positive

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello, World!");
+const balance =  document.getElementById('balance'),
+moneyPlus =  document.getElementById('money-plus'),
+moneyMinus =  document.getElementById('money-minus'),
+list  =  document.getElementById('list'),
+form =  document.getElementById('form'),
+text =  document.getElementById('text'),
+amount =  document.getElementById('amount'),
+dummyTransactions =  [
+    {id: 1, text: 'flower', amount: -20},
+    {id: 2, text: 'Salary', amount: 300},
+    {id: 3, text: 'Book', amount: -50},
+    {id: 4, text: 'Camera', amount: 150}
+];
+
+let transactions = dummyTransactions;
+
+//Add Transactions to DOM list
+
+function addTransactionDom(transaction) {
+    //Get Sign
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+    const item = document.createElement('li');
+    //Add class based on sign
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'add');
+    item.innerHTML = `
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+        <button class="delete-btn">x</button>
+    `;
+
+}
+
+addTransactionDom(transactions);
